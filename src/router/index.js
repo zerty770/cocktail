7122b68040ac9ae9d2e8aea2a487a9ed60190fc9
Yechouroun ariche <yechouroun.a@gmail.com>
2022-07-13 17:06:34 +0200

    main

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import * as Public from '@/views/public/index'
+
+import * as Admin from '@/views/admin/index'
+
+
+
+
+
 
 const routes = [
+
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path:'/',
+    name:'public',
+    component:Public.PublicLayout,
+    children:[
+      { path:'/', name:'home', component: Public.Home},
+      { path:'/cocktails', name:'Cocktails', component: Public.Cocktail},
+      { path:'/contact', name:'Contact', component: Public.Contact}
+
+    ]
+  },  
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin.AdminLayout,
+    children:[
+      {path: 'dashboard', name:'Dashboard', component: Admin.Dashboard },
+      {path: 'users/Index', name:'UserIdex', component: Admin.UserIndex },
+      {path: 'users/Edit/:id', name:'UserEdit', component: Admin.UserEdit },
+      {path: 'users/add', name:'UserAdd', component:Admin.UserAdd },
+
+      {path: 'cocktails/Index', name:'CocktailIdex', component: Admin.CocktailIndex },
+      {path: 'cocktails/Edit', name:'CocktailEdit', component: Admin.CocktailEdit },
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/:pathMatch(.*)*', redirect: '/'
   }
+
 ]
 
 const router = createRouter({
